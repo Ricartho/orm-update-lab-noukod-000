69051674044152d6d0d6eb6eb30c9529962471ce
Ricartho Franck <ricartho.franck@esih.edu>
2019-05-20 20:57:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,8 +48,8 @@ def save
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
   
-  def self.create
-    
+  def self.create(name,grade)
+    Student.new(name,grade)
     
   end 
 end

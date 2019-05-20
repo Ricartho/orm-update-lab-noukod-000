e64c21c692b72675b1ef2a1461bb7ba596fbc72f
Ricartho Franck <ricartho.franck@esih.edu>
2019-05-20 20:54:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,21 +28,22 @@ def self.drop_table
   SQL
   DB[:conn].execute(sql)
 end 
- def self.new_from_db(row)
-    # create a new Student object given a row from the database
-    new_student = self.new
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-    new_student
-  end
 
-# def save
-#     sql = <<-SQL
-#       INSERT INTO students (name, grade) 
-#       VALUES (?, ?)
-#     SQL
-
-#     DB[:conn].execute(sql, self.name, self.grade)
+# def self.new_from_db(row)
+#     # create a new Student object given a row from the database
+#     new_student = self.new
+#     new_student.id = row[0]
+#     new_student.name = row[1]
+#     new_student.grade = row[2]
+#     new_student
 #   end
+
+def save
+    sql = <<-SQL
+      INSERT INTO students (name, grade) 
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+  end
 end

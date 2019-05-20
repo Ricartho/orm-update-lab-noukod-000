4fdb9341842bfa921426ec034b78cb5d32f5adfb
Ricartho Franck <ricartho.franck@esih.edu>
2019-05-20 21:13:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,10 +15,10 @@ class Student
 
   def self.create_table
     sql = <<-SQL
-                  CREATE TABLE IF NOT EXISTS students (
-                  id INTEGER PRIMARY KEY,
-                  name TEXT,
-                  grade TEXT
+      CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
                 )
     SQL
 
@@ -26,10 +26,7 @@ class Student
   end
 
   def self.drop_table
-    sql = <<-SQL
-                DROP TABLE IF EXISTS students
-    SQL
-
+    sql ="DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
 
@@ -37,10 +34,7 @@ class Student
     if self.id
         self.update
     else
-      sql = <<-SQL
-       INSERT INTO students (name, grade) VALUES (?, ?)
-      SQL
-
+      sql = "INSERT INTO students (name, grade) VALUES (?, ?)"
       DB[:conn].execute(sql, self.name, self.grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students").flatten.first
     end

2bd88479d287e1eea92593442c8c634b87b430f5
Ricartho Franck <ricartho.franck@esih.edu>
2019-05-20 20:36:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ class Student
 
 def self.create_table
   sql = <<-SQL
-  CREATE IF NOT EXISTS TABLE students(
+  CREATE  TABLE  IF NOT EXISTS students(
   id INTEGER PRIMARY KEY,
   name TEXT,
   grade INTEGER
